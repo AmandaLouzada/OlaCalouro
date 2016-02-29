f709b39d7164456f2ea78d6cf2102c2ace996e81
Amanda Louzada <amandanuneslouzada@gmail.com>
2016-02-29 03:22:02 -0300

    Adicionanda constantes de tipo do local

--- app/src/main/java/louzada/olacalouro/Activity/Constantes.java
+++ app/src/main/java/louzada/olacalouro/Activity/Constantes.java
@@ -8,5 +8,15 @@ public class Constantes {
     public static final int CATEGORIA_UFT= 1;
     public static final int CATEGORIA_PALMAS = 2;
     public static final int CATEGORIA_UTILIDADES = 3;
+    public static final int BLOCO = 1;
+    public static final int SALA = 2;
+    public static final int DIRETORIA = 3;
+    public static final int PROREITORIA = 4;
+    public static final int ALIMENTACAO = 5;
+    public static final int ONIBUS = 6;
+    public static final int LABORATORIO = 7;
+    public static final int BIBLIOTECA = 8;
+    public static final int BANCO = 9;
+    public static final int PRAIA = 10;
 
 }
